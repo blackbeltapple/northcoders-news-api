e26497acd44aa035c53ccd62a01d45797d31d7ef
DaveG437 <davegardiner1@hotmail.com>
2016-12-01 12:46:23 +0000

    Add getArticlesForTopic and getArticles routes and controllers

---
+++ seed/data/findArticles.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const articleDoc = require('../../models/articles.js');
+
+mongoose.connect('mongodb://localhost/northcoders-news-api', function(error) {
+  if (error) {
+    console.log(error);
+    process.exit();
+  }
+
+  articleDoc.find(function(error, data){
+    if(error){
+      console.error(error);
+    }
+    console.log(data + '\n number of data entries: ' + data.length);
+    process.exit();
+  })
+});
