a39f74ffece5b0f037bf4ae4f22ab543134429cc
DaveG437 <davegardiner1@hotmail.com>
2016-12-01 13:16:28 +0000

    Add getCommentsForArticle route and controlloer

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -1,3 +1,42 @@
-/**
- * Created by dave on 01/12/16.
- */
+const topicDoc = require ('../models/topics.js');
+const articleDoc = require ('../models/articles.js');
+const commentDoc = require ('../models/comments.js');
+
+
+
+const getAllTopics = function (callback) {
+  topicDoc.find({}, function(error, docs){
+    if (error) return callback(error);
+    callback(null, docs);
+  })
+}
+
+const getArticlesForTopic = function (topic, callback) {
+  articleDoc.find({belongs_to: topic}, function(error, docs){
+    if (error) return callback(error);
+    callback(null, docs);
+  })
+}
+
+const getArticles = function (callback) {
+  articleDoc.find(function(error, docs){
+    if (error) return callback(error);
+    callback(null, docs);
+  })
+}
+
+const getCommentsForArticle = function (article, callback) {
+  commentDoc.find({belongs_to: article}, function(error, docs){
+    if (error) return callback(error);
+    callback(null, docs);
+  })
+}
+
+module.exports = {
+  getAllTopics: getAllTopics,
+  getArticlesForTopic: getArticlesForTopic,
+  getArticles: getArticles,
+  getCommentsForArticle: getCommentsForArticle
+
+
+}
--- Routes/api.js
+++ Routes/api.js
@@ -10,4 +10,30 @@ apiRouter.get('/topics', function(req, res){
   });
 });
 
+apiRouter.get('/topics/:topic_id/articles', function(req, res){
+  var topic = req.params.topic_id;
+  console.log('you have requested all articles for ' + topic);
+  controllers.getArticlesForTopic(topic, function(error, data){
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
+});
+
+apiRouter.get('/articles', function(req, res){
+  console.log('you have requested articles');
+  controllers.getArticles(function(error, data){
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
+});
+
+apiRouter.get('/articles/:article_id/comments', function(req, res){
+  var article = req.params.article_id;
+  console.log('you have requested comments for ' + article);
+  controllers.getCommentsForArticle(article, function(error, data){
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
+});
+
 module.exports = apiRouter;
