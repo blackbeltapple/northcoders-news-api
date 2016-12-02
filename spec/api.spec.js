a0251acf18f2ebd85a5829e1a067c05ed9e2b435
amanda manekshaw <amanda_manekshaw@hotmail.com>
2016-12-02 21:42:36 +0000

    Added ROOT variable in api.spec.js ROOT = `http://localhost:${PORT}/api`

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,20 +1,26 @@
 // need to make sure our test file runs in TEST env models
 process.env.NODE_ENV = 'test';
 const PORT = require('../config.js').PORT[process.env.NODE_ENV];
-//the above const is an environment variable which means it can be used anywhere in the project. So, the fact that
-// PORT is unused in this file doesn't matter, it is used below by require('../server.js');
+// The above const is an environment variable
+// PORT is unused in this file, but is used in server.js
 
 const expect = require('chai').expect;
 const request = require('supertest');
 const mongoose = require('mongoose');
-
 const saveTestData = require('../seed/test.seed.js');
 
+// PORT = 3090
+const ROOT = `http://localhost:${PORT}/api`;
+// ROOT = http://localhost:3090/api
+
 // get the server up and running - in TEST env
+// this require pulls in the code from server.js AND EXECUTES IT
 require('../server.js');
 
 describe('API Routes', function () {
   var usefulIds;
+  // the before 'hook' runs before the whole test suite
+  // (if you want one to run before every test then you should use Before Each)
   before(function (done) {
     mongoose.connection.once('connected', function () {
       mongoose.connection.db.dropDatabase();
@@ -25,11 +31,10 @@ describe('API Routes', function () {
       })
     })
   });
-
-  //This first test is a bit of a wrong 'un
+  // This route isn't supported but will test
   describe('GET /api', function () {
     it('should return status 404', function (done) {
-      request('http://localhost:3090/api')
+      request(ROOT)
         .get('/')
         .expect(404)
         .end(function (err, res) {
@@ -40,10 +45,10 @@ describe('API Routes', function () {
         });
     });
   });
-
   describe('GET /api/topics', function () {
     it('should return status 200 and body of topic', function (done) {
-      request('http://localhost:3090/api')
+      //      request('http://localhost:3090/api')  // this is equivalent to request(ROOT)
+      request(ROOT)
         .get('/topics')
         .expect(200)
         .end(function (err, res) {
@@ -59,10 +64,10 @@ describe('API Routes', function () {
         });
     });
   });
-
   describe('GET /api/topiks', function () {
     it('should return status 404', function (done) {
-      request('http://localhost:3090/api')
+      //      request('http://localhost:3090/api')  // this is equivalent to request(ROOT)
+      request(ROOT)
         .get('/topiks')
         .end(function (err, res) {
           if (err) throw err;
@@ -71,10 +76,10 @@ describe('API Routes', function () {
         });
     });
   });
-
   describe('GET /api/topics/:topic_id/articles', function () {
     it('should return status 200 and body with array of articles', function (done) {
-      request('http://localhost:3090/api')
+      //      request('http://localhost:3090/api')  // this is equivalent to request(ROOT)
+      request(ROOT)
         .get('/topics/cats/articles')
         .expect(200)
         .end(function (err, res) {
@@ -86,10 +91,10 @@ describe('API Routes', function () {
         });
     });
   });
-
   describe('GET /api/topics/:(unknown)topic_id/articles', function () {
     it('should return status 404', function (done) {
-      request('http://localhost:3090/api')
+      //      request('http://localhost:3090/api')  // this is equivalent to request(ROOT)
+      request(ROOT)
         .get('/topics/ca/articles')
         .end(function (err, res) {
           if (err) throw err;
@@ -100,11 +105,11 @@ describe('API Routes', function () {
         });
     });
   });
-
   describe('POST /api/articles/:article_id/comments (valid article, valid comment obj)', function () {
     it('should return status 200 and {status: OK}', function (done) {
       var tmpString = `/articles/${usefulIds.article_id}/comments`;
-      request('http://localhost:3090/api')
+      //      request('http://localhost:3090/api')  // this is equivalent to request(ROOT)
+      request(ROOT)
         .post(tmpString) //.post and .send work together. .post is saying you want to
         .send({body: 'test comment', belongs_to: usefulIds.article_id })                   // do a post, and the .send is the body that you want to send!
         // .expect(200)
@@ -114,27 +119,26 @@ describe('API Routes', function () {
           done();
         });
     });
-
-    // Should we be handling this error in our controller?
-    describe('POST /api/articles/:article_id/comments (valid article, invalid comment obj)', function () {
-      xit('should return status 500 ??', function (done) {
-        var tmpString = `/articles/${usefulIds.article_id}/comments`;
-        request('http://localhost:3090/api')
-          .post(tmpString) //.post and .send work together. .post is saying you want to
-          .send({comment: 'test comment' })                   // do a post, and the .send is the body that you want to send!
-          // .expect(200)
-          .end(function (err, res) {
-            if (err) throw err;
-            expect(res.statusCode).to.equal(400);
-            done();
-          });
-      });
+  })
+  // Should we be handling this error in our controller?
+  describe('POST /api/articles/:article_id/comments (valid article, invalid comment obj)', function () {
+    xit('should return status 500 ??', function (done) {
+      var tmpString = `/articles/${usefulIds.article_id}/comments`;
+      //      request('http://localhost:3090/api')  // this is equivalent to request(ROOT)
+      request(ROOT)
+        .post(tmpString) //.post and .send work together. .post is saying you want to
+        .send({comment: 'test comment' })                   // do a post, and the .send is the body that you want to send!
+        // .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+          expect(res.statusCode).to.equal(400);
+          done();
+        });
+    });
   });
-
   after(function (done) {
     // clear the DB
     mongoose.connection.db.dropDatabase();
     done();
   });
-})
 })
