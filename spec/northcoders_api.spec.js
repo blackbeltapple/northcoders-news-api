5bec916f1c24288550ed2f2f812f1968688002e4
DaveG437 <davegardiner1@hotmail.com>
2016-12-02 15:53:13 +0000

    Complete first five tests.

--- spec/northcoders_api.spec.js
+++ spec/northcoders_api.spec.js
@@ -1,6 +1,8 @@
 // need to make sure our test file runs in TEST env models
 process.env.NODE_ENV = 'test';
 const PORT = require('../config.js').PORT[process.env.NODE_ENV];
+//the above const is an environment variable which means it can be used anywhere in the project. So, the fact that
+// PORT is unused in this file doesn't matter, it is used below by require('../server.js');
 
 const expect = require('chai').expect;
 const request = require('supertest');
@@ -24,6 +26,7 @@ describe('API Routes', function () {
     })
   });
 
+  //This first test is a bit of a wrong 'un
   describe('GET /api', function () {
     it('should return status 404', function (done) {
       request('http://localhost:3090/api')
@@ -39,15 +42,60 @@ describe('API Routes', function () {
   });
 
   describe('GET /api/topics', function () {
-    it('should return status 200', function (done) {
-      console.log()
+    it('should return status 200 and body of topic', function (done) {
       request('http://localhost:3090/api')
         .get('/topics')
         .expect(200)
         .end(function (err, res) {
           if (err) throw err;
           expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body.length).to.equal(3);
           expect(res.body[0].title).to.equal('Football');
+          res.body.map(function(element){
+            expect(element).to.have.all.keys('title', 'slug', '__v', '_id');
+          })
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/topiks', function () {
+    it('should return status 404', function (done) {
+      request('http://localhost:3090/api')
+        .get('/topiks')
+        .end(function (err, res) {
+          if (err) throw err;
+          expect(res.statusCode).to.equal(404);
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/topics/:topic_id/articles', function () {
+    it('should return status 200 and body with array of articles', function (done) {
+      request('http://localhost:3090/api')
+        .get('/topics/cats/articles')
+        .expect(200)
+        .end(function (err, res) {
+          if (err) throw err;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body.length).to.equal(1);
+          done();
+        });
+    });
+  });
+
+  describe('GET /api/topics/:(unknown)topic_id/articles', function () {
+    it('should return status 404', function (done) {
+      request('http://localhost:3090/api')
+        .get('/topics/ca/articles')
+        .end(function (err, res) {
+          if (err) throw err;
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('array');
+          expect(res.body.length).to.equal(0);
           done();
         });
     });
@@ -56,8 +104,8 @@ describe('API Routes', function () {
   describe('POST /api/articles/:article_id/comments', function () {
     xit('should return status 200 and {status: OK}', function (done) {
       request('http://localhost:3090/api')
-        .post('/articles/${usefulIds.article_id}/comments')
-        .send({comment: 'test comment'})
+        .post('/articles/${usefulIds.article_id}/comments')//.post and .send work together. .post is saying you want to
+        .send({comment: 'test comment'})                   // do a post, and the .send is the body that you want to send!
         // .expect(200)
         .end(function (err, res) {
           if (err) throw err;
